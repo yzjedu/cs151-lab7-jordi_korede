aa09a7d3fecaab11e21f22e8349b551970beb396
oaoni <oaoni@loyola.edu>
2024-10-29 18:55:10 -0400

    Finished the code (except for comments)

--- main.py
+++ main.py
@@ -10,41 +10,58 @@
 # Data Out: The cost of each room and the total cost for the house
 # Credits: Class
 
-def get_dimensions():
-    width=int(input("Enter width: "))
+def get_dimensions(room_display):
+    width = int(input("Enter the width of Room " + str(room_display) + ": "))
     while width < 0:
-        print("Enter a positive number")
-    length=int(input("Enter length: "))
+        width = int(input("Invalid width. Please try again with a positive number. "))
+    length = int(input("Enter the length of Room " + str(room_display) + ": "))
     while length < 0:
-        print("Enter a positive number")
-    area = width*length
+        length = int(input("Invalid length. Please try again with a positive number. "))
+    area = width * length
     return area
-def get_flooring():
-    flooring=int(input("Enter flooring: (hardwood,carpet,or tile) "))
+
+def get_flooring(room_display):
+    flooring = str(input("What flooring will be used for Room " + str(room_display)  + "? (Please choose between hardwood, carpet, or tile flooring) "))
     flooring = flooring.lower()
     while flooring != "hardwood" and flooring != "carpet" and flooring != "tile":
-        print("Enter either hardwood,carpet, or tile ")
+        flooring = str(input("Invalid option. Please choose between hardwood, carpet, or tile flooring. "))
     return flooring
+
 def room_cost(area, flooring):
+    this_room = 0
     if flooring == "hardwood":
-        room_cost = area * 1.39
+        this_room = area * 1.39
     elif flooring == "carpet":
-        room_cost = area * 3.99
+        this_room = area * 3.99
     elif flooring == "tile":
-        room_cost = area * 4.99
-    return room_cost
-def total_cost(cost1,cost2,cost3,cost4,cost5):
-    total_cost = cost1 + cost2 + cost3 + cost4 + cost5
-    print(total_cost)
+        this_room = area * 4.99
+    return this_room
+
 
 def main():
-    room1 = room_cost(get_dimensions(), get_flooring())
+    add_room = 0
+    final_cost = 0
+    keep_count = 1
+    print("Hello! Welcome to our flooring price calculator!\n"
+          "In this program, we'll be calculating the amount your flooring will cost!\n"
+          "All you need to do is enter the dimensions of each room and the\n"
+          "flooring for each one.\n"
+          "Let's get started!")
+    while add_room < 5:
+        print("")
+        print("Room " + str(keep_count))
+        display_cost = room_cost(get_dimensions(keep_count), get_flooring(keep_count))
+        print("Room " + str(keep_count) + f" will cost ${display_cost:.2f}")
+        final_cost += display_cost
+        add_room += 1
+        keep_count+=1
+    print("")
+    print("Alright, we've gotten all we need and we have your final cost!")
+    print(f"The final cost for all the flooring you need is: ${final_cost:.2f}")
+    print("")
+    print("")
+    print("Thank you for using our flooring price calculator! :)")
+    print("")
 
 
-
-
-
-
-
-
-
+main()
